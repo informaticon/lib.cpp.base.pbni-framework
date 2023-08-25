08e7abd9fc3ce25c9e29bb285081757956deaea1
Micha Wehrli <micha.wehrli@informaticon.com>
2023-08-25 18:29:49 +0200

    add indentation

--- src/PBTypes.h
+++ src/PBTypes.h
@@ -47,151 +47,151 @@ namespace Inf
      */
 
 #pragma region Any
-template<>
-struct Type<PBAny>
-{
-    static constexpr wchar_t PBSignature = L'A';
-    static constexpr pbvalue_type PBType = pbvalue_any;
-    static inline std::wstring GetPBName(std::wstring argument_name) { return L"any" + argument_name; }
-};
+    template<>
+    struct Type<PBAny>
+    {
+        static constexpr wchar_t PBSignature = L'A';
+        static constexpr pbvalue_type PBType = pbvalue_any;
+        static inline std::wstring GetPBName(std::wstring argument_name) { return L"any" + argument_name; }
+    };
 #pragma endregion
 
 #pragma region Bytelike
-template<>
-struct Type<PBByte>
-{
-    static constexpr wchar_t PBSignature = L'E';
-    static constexpr pbvalue_type PBType = pbvalue_byte;
-    static inline std::wstring GetPBName(std::wstring argument_name) { return L"byte" + argument_name; }
-};
+    template<>
+    struct Type<PBByte>
+    {
+        static constexpr wchar_t PBSignature = L'E';
+        static constexpr pbvalue_type PBType = pbvalue_byte;
+        static inline std::wstring GetPBName(std::wstring argument_name) { return L"byte" + argument_name; }
+    };
 
-template<>
-struct Type<PBBoolean>
-{
-    static constexpr wchar_t PBSignature = L'B';
-    static constexpr pbvalue_type PBType = pbvalue_boolean;
-    static inline std::wstring GetPBName(std::wstring argument_name) { return L"boolean" + argument_name; }
-};
+    template<>
+    struct Type<PBBoolean>
+    {
+        static constexpr wchar_t PBSignature = L'B';
+        static constexpr pbvalue_type PBType = pbvalue_boolean;
+        static inline std::wstring GetPBName(std::wstring argument_name) { return L"boolean" + argument_name; }
+    };
 
-template<>
-struct Type<PBChar>
-{
-    static constexpr wchar_t PBSignature = L'H';
-    static constexpr pbvalue_type PBType = pbvalue_char;
-    static inline std::wstring GetPBName(std::wstring argument_name) { return L"char" + argument_name; }
-};
+    template<>
+    struct Type<PBChar>
+    {
+        static constexpr wchar_t PBSignature = L'H';
+        static constexpr pbvalue_type PBType = pbvalue_char;
+        static inline std::wstring GetPBName(std::wstring argument_name) { return L"char" + argument_name; }
+    };
 #pragma endregion
 
 #pragma region Integer_Numbers
-template<>
-struct Type<PBInt>
-{
-    static constexpr wchar_t PBSignature = L'I';
-    static constexpr pbvalue_type PBType = pbvalue_int;
-    static inline std::wstring GetPBName(std::wstring argument_name) { return L"int" + argument_name; }
-};
+    template<>
+    struct Type<PBInt>
+    {
+        static constexpr wchar_t PBSignature = L'I';
+        static constexpr pbvalue_type PBType = pbvalue_int;
+        static inline std::wstring GetPBName(std::wstring argument_name) { return L"int" + argument_name; }
+    };
 
-template<>
-struct Type<PBUint>
-{
-    static constexpr wchar_t PBSignature = L'N';
-    static constexpr pbvalue_type PBType = pbvalue_uint;
-    static inline std::wstring GetPBName(std::wstring argument_name) { return L"uint" + argument_name; }
-};
+    template<>
+    struct Type<PBUint>
+    {
+        static constexpr wchar_t PBSignature = L'N';
+        static constexpr pbvalue_type PBType = pbvalue_uint;
+        static inline std::wstring GetPBName(std::wstring argument_name) { return L"uint" + argument_name; }
+    };
 
-template<>
-struct Type<PBLong>
-{
-    static constexpr wchar_t PBSignature = L'L';
-    static constexpr pbvalue_type PBType = pbvalue_long;
-    static inline std::wstring GetPBName(std::wstring argument_name) { return L"long" + argument_name; }
-};
+    template<>
+    struct Type<PBLong>
+    {
+        static constexpr wchar_t PBSignature = L'L';
+        static constexpr pbvalue_type PBType = pbvalue_long;
+        static inline std::wstring GetPBName(std::wstring argument_name) { return L"long" + argument_name; }
+    };
 
-template<>
-struct Type<PBUlong>
-{
-    static constexpr wchar_t PBSignature = L'U';
-    static constexpr pbvalue_type PBType = pbvalue_ulong;
-    static inline std::wstring GetPBName(std::wstring argument_name) { return L"ulong" + argument_name; }
-};
+    template<>
+    struct Type<PBUlong>
+    {
+        static constexpr wchar_t PBSignature = L'U';
+        static constexpr pbvalue_type PBType = pbvalue_ulong;
+        static inline std::wstring GetPBName(std::wstring argument_name) { return L"ulong" + argument_name; }
+    };
 
-template<>
-struct Type<PBLongLong>
-{
-    static constexpr wchar_t PBSignature = L'K';
-    static constexpr pbvalue_type PBType = pbvalue_longlong;
-    static inline std::wstring GetPBName(std::wstring argument_name) { return L"longlong" + argument_name; }
-};
+    template<>
+    struct Type<PBLongLong>
+    {
+        static constexpr wchar_t PBSignature = L'K';
+        static constexpr pbvalue_type PBType = pbvalue_longlong;
+        static inline std::wstring GetPBName(std::wstring argument_name) { return L"longlong" + argument_name; }
+    };
 #pragma endregion
 
 #pragma region Floating_Point_Numbers
-template<>
-struct Type<PBReal>
-{
-    static constexpr wchar_t PBSignature = L'F';
-    static constexpr pbvalue_type PBType = pbvalue_real;
-    static inline std::wstring GetPBName(std::wstring argument_name) { return L"real" + argument_name; }
-};
+    template<>
+    struct Type<PBReal>
+    {
+        static constexpr wchar_t PBSignature = L'F';
+        static constexpr pbvalue_type PBType = pbvalue_real;
+        static inline std::wstring GetPBName(std::wstring argument_name) { return L"real" + argument_name; }
+    };
 
-template<>
-struct Type<PBDouble>
-{
-    static constexpr wchar_t PBSignature = L'D';
-    static constexpr pbvalue_type PBType = pbvalue_double;
-    static inline std::wstring GetPBName(std::wstring argument_name) { return L"double" + argument_name; }
-};
+    template<>
+    struct Type<PBDouble>
+    {
+        static constexpr wchar_t PBSignature = L'D';
+        static constexpr pbvalue_type PBType = pbvalue_double;
+        static inline std::wstring GetPBName(std::wstring argument_name) { return L"double" + argument_name; }
+    };
 
-template<>
-struct Type<PBDecimal>
-{
-    static constexpr wchar_t PBSignature = L'M';
-    static constexpr pbvalue_type PBType = pbvalue_dec;
-    static inline std::wstring GetPBName(std::wstring argument_name) { return L"dec" + argument_name; }
-};
+    template<>
+    struct Type<PBDecimal>
+    {
+        static constexpr wchar_t PBSignature = L'M';
+        static constexpr pbvalue_type PBType = pbvalue_dec;
+        static inline std::wstring GetPBName(std::wstring argument_name) { return L"dec" + argument_name; }
+    };
 #pragma endregion
 
 #pragma region Date_Times
-template<>
-struct Type<PBTime>
-{
-    static constexpr wchar_t PBSignature = L'T';
-    static constexpr pbvalue_type PBType = pbvalue_time;
-    static inline std::wstring GetPBName(std::wstring argument_name) { return L"time" + argument_name; }
-};
+    template<>
+    struct Type<PBTime>
+    {
+        static constexpr wchar_t PBSignature = L'T';
+        static constexpr pbvalue_type PBType = pbvalue_time;
+        static inline std::wstring GetPBName(std::wstring argument_name) { return L"time" + argument_name; }
+    };
 
-template<>
-struct Type<PBDate>
-{
-    static constexpr wchar_t PBSignature = L'Y';
-    static constexpr pbvalue_type PBType = pbvalue_date;
-    static inline std::wstring GetPBName(std::wstring argument_name) { return L"date" + argument_name; }
-};
+    template<>
+    struct Type<PBDate>
+    {
+        static constexpr wchar_t PBSignature = L'Y';
+        static constexpr pbvalue_type PBType = pbvalue_date;
+        static inline std::wstring GetPBName(std::wstring argument_name) { return L"date" + argument_name; }
+    };
 
-template<>
-struct Type<PBDateTime>
-{
-    static constexpr wchar_t PBSignature = L'W';
-    static constexpr pbvalue_type PBType = pbvalue_datetime;
-    static inline std::wstring GetPBName(std::wstring argument_name) { return L"datetime" + argument_name; }
-};
+    template<>
+    struct Type<PBDateTime>
+    {
+        static constexpr wchar_t PBSignature = L'W';
+        static constexpr pbvalue_type PBType = pbvalue_datetime;
+        static inline std::wstring GetPBName(std::wstring argument_name) { return L"datetime" + argument_name; }
+    };
 #pragma endregion
 
 #pragma region Variable_Sized
-template<>
-struct Type<PBString>
-{
-    static constexpr wchar_t PBSignature = L'S';
-    static constexpr pbvalue_type PBType = pbvalue_string;
-    static inline std::wstring GetPBName(std::wstring argument_name) { return L"string" + argument_name; }
-};
+    template<>
+    struct Type<PBString>
+    {
+        static constexpr wchar_t PBSignature = L'S';
+        static constexpr pbvalue_type PBType = pbvalue_string;
+        static inline std::wstring GetPBName(std::wstring argument_name) { return L"string" + argument_name; }
+    };
 
-template<>
-struct Type<PBBlob>
-{
-    static constexpr wchar_t PBSignature = L'O';
-    static constexpr pbvalue_type PBType = pbvalue_blob;
-    static inline std::wstring GetPBName(std::wstring argument_name) { return L"blob" + argument_name; }
-};
+    template<>
+    struct Type<PBBlob>
+    {
+        static constexpr wchar_t PBSignature = L'O';
+        static constexpr pbvalue_type PBType = pbvalue_blob;
+        static inline std::wstring GetPBName(std::wstring argument_name) { return L"blob" + argument_name; }
+    };
 
     /**
      * For Types that have their own template types (PBObject<>, PBArray<>) we cant partially specialize the static Functions.
